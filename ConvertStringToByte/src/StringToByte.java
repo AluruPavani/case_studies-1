c0fd269b1d1bca23b57d04938ddd6f6ff843d48e
AluruPavani <69709115+AluruPavani@users.noreply.github.com>
2021-05-17 10:39:08 +0530

    Update StringToByte.java

--- ConvertStringToByte/src/StringToByte.java
+++ ConvertStringToByte/src/StringToByte.java
@@ -1,18 +1,4 @@
-/*
-public class StringToByte {
 
-	public static byte convertStringTobyte(String str) {
-		return Byte.parseByte(str);
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String stringValue="5";
-		byte byteValue=convertStringTobyte(stringValue);
-		System.out.println(stringValue+ " after converting into byte = " +byteValue);
-
-	}
-
-}*/
 import java.util.Arrays;
 public class StringToByte{
 	public static void main(String[] args) {
